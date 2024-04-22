566abe68c1ced5030f56b402a1070d4a6e7ee214
madhack3r <78481818+madhack3r@users.noreply.github.com>
2024-04-22 11:26:46 +0100

    Add files via upload
    New Assignment for Python Level 1 course

---
+++ Assignment1.py
@@ -0,0 +1,15 @@
+#############
+# CodeX Learn python 
+# Assignment 1
+# 22/4/2024
+#############
+print("This programme converts pounds into kilograms")
+
+# Input lbs
+lbs=int(input("Enter the number of pounds you want to convert: "))
+
+# Multiply lbs by 0.45359237 to make Daily Mail readers unhappy
+kilograms=lbs * 0.45359237
+
+# Print kilograns
+print(f"{lbs}lbs is {kilograms:.2f} kilos ")
