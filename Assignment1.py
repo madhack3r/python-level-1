eb27226bc1c6c2b5119399475eec62f947d4b85a
madhack3r <78481818+madhack3r@users.noreply.github.com>
2024-04-22 14:58:14 +0100

    Update Assignment1.py
    Updated without intermediate variable for conversion

--- Assignment1.py
+++ Assignment1.py
@@ -9,7 +9,11 @@ print("This programme converts pounds into kilograms")
 lbs=int(input("Enter the number of pounds you want to convert: "))
 
 # Multiply lbs by 0.45359237 to make Daily Mail readers unhappy
+# explicit conversion 
 kilograms=lbs * 0.45359237
 
 # Print kilograns
 print(f"{lbs}lbs is {kilograms:.2f} kilos ")
+
+# Print without a intermediate variable 
+# print(f"{lbs} lbs is {lbs * 0.45359237:.2f} kilos ")
