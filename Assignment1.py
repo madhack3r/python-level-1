29338c43189b6a4f8f60b90b0025bc8ddf4b0885
madhack3r <78481818+madhack3r@users.noreply.github.com>
2024-04-23 15:01:45 +0100

    Update Assignment1.py
    Completed optional assignment

--- Assignment1.py
+++ Assignment1.py
@@ -1,19 +1,21 @@
-#############
-# CodeX Learn python 
-# Assignment 1
-# 22/4/2024
-#############
-print("This programme converts pounds into kilograms")
+###############################
+# CodeX Learn python          #
+# Assignment 1                #
+# 22/4/2024                   #
+###############################
 
-# Input lbs
-lbs=int(input("Enter the number of pounds you want to convert: "))
+print("Welcome to the Weight Converter!\n")
 
-# Multiply lbs by 0.45359237 to make Daily Mail readers unhappy
-# explicit conversion 
-kilograms=lbs * 0.45359237
-
-# Print kilograns
-print(f"{lbs}lbs is {kilograms:.2f} kilos ")
-
-# Print without a intermediate variable 
-# print(f"{lbs} lbs is {lbs * 0.45359237:.2f} kilos ")
+while True:
+  try:
+    # Input lbs
+    kgs=float(input("Enter the number of kilograms you want to convert: "))
+    break
+  except:
+    print("Invalid input. Please enter a numeric value.\n")
+  
+# Multiply kgs by 2.20462 to make Daily Mail readers happy
+print(f"{kgs} kgs is equivalent to {kgs * 2.20462:.2f} pounds ")
+# 100.5 kgs is equivalent to 221.56 pounds or 221.56431 without rounding ????? 
+# Example 2 states that 
+# 100.5 kg is equivalent to 221.66 pounds
