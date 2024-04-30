1ef9a250611a22df050880caa56084a089a16fcc
madhack3r <78481818+madhack3r@users.noreply.github.com>
2024-04-30 16:01:06 +0100

    Update Assignment2.py
    Removed debug

--- Assignment2.py
+++ Assignment2.py
@@ -4,15 +4,18 @@
 # 29/4/2024                   #
 ###############################
 
+# Debug Mode - set to True to print debug messages
+debug_mode = False
+
+
 print("Welcome to the Prime Evaluator!\n")
 
-
 # Define a prime indicator and set to false by default
-isTrue = False
+isPrime = False
 
 while True:
   try:
-    number=int(input("Please enter a number (enter \"Stop\" to end) !\n"))
+    number=int(input("Please enter an integer number to test for primeness (enter \"Stop\" to end) !\n"))
 
     # If the number is 1 then it is not a prime - outlier case
     if number == 1:
@@ -20,18 +23,25 @@ while True:
     elif number > 1:
         # Prime check - loop through all divisors from 2 to number
         for i in range(2, number):
-            # print(f"Value of i is : {i} \n")
-            
+            # Debug
+            if (debug_mode):
+              print(f"Value of i is : {i} \n")
+
             # Divide by integer and if the remainder is 0 
             # then it is not a prime as it can be divide by 
             # a number apart from 1 and itself
             if (number % i) == 0:
-                # If factor is found, set flag to True and break out of loop
-                isTrue = True
-                break
+              
+              # ********** Debug ***********
+              if (debug_mode):
+                print(f"number % i is {number % i} \n")
+              
+              # If factor is found, set flag to True and break out of loop
+              isPrime = True
+              break
         
         # Check if flag is True
-        if isTrue:
+        if (isPrime):
             print(f"{number} is not a prime number\n")
         else:
             print(f"{number} is a prime number\n")
